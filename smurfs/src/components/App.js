4e67412dfe8a7814ba52e0771b8b6dcefc49d95a
Philip Johnson <phil7j@gmail.com>
2019-07-26 10:33:12 -0700

    inital setup od contextAPI with hooks

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,22 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { SmurfContext } from './contexts';
+import Smurfs from './Smurfs';
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
+import { useSmurfsFetch } from './hooks/getSmurfs';
+function App() {
+
+   const [smurfs] = useSmurfsFetch('http://localhost:3333/smurfs', [])
+
+   return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <SmurfContext.Provider value={smurfs}>
+        <Smurfs />
+        <Form />
+        </SmurfContext.Provider>
       </div>
     );
   }
-}
+
 
 export default App;
---
+++ smurfs/src/components/Smurfs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Smurfs() {
+    return (
+        <div>
+            <p>Smurfs List</p>
+        </div>
+    )
+}
+
+export default Smurfs
